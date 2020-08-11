4e78aa0c1f745b223542e163e14de2bbb2485653
Shubham Bansal <bansall.shubham@gmail.com>
2020-08-11 22:59:00 +0530

    Updates

---
+++ precache-manifest.53fd394887f1b1c4d03f401af266a401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea0f841f3494ff5f01c66212eea409d",
+    "url": "/react-app-monster-rolodex/index.html"
+  },
+  {
+    "revision": "83c5b33e3da29639d517",
+    "url": "/react-app-monster-rolodex/static/css/main.35c7c237.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/react-app-monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c5b33e3da29639d517",
+    "url": "/react-app-monster-rolodex/static/js/main.8d6f0a65.chunk.js"
+  },
+  {
+    "revision": "7a8d4ccb818d1a48db3f",
+    "url": "/react-app-monster-rolodex/static/js/runtime-main.703d55c1.js"
+  }
+]);
